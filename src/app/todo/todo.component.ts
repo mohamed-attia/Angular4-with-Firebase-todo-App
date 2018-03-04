a2448dda42d85143ca14b33d0156ac88f3b8250f
Mohamed--Attia <Mohamed@DESKTOP-D9NMD2M.Naseejdev.loc>
2018-03-04 14:13:16 +0200

    done

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo/shared/todo.service';
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
+})
+export class TodoComponent implements OnInit {
+  toDoListArray: any[];
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+    this.retriveData();
+  }
+
+  retriveData(){
+    this.todoService.getToDoList().snapshotChanges()
+    .subscribe(res => {
+      this.toDoListArray = [];
+      //console.log("res",res)
+      res.forEach(element => {
+      //  console.log("element",element)
+       var x = element.payload.toJSON();
+       x["$key"] = element.key;
+       this.toDoListArray.push(x);
+      // console.log("x",x)
+      })
+
+      //sort array isChecked false  -> true
+        this.toDoListArray.sort((a,b) => {
+          return a.isChecked - b.isChecked;
+        })
+    });
+  }//end retriveData
+
+  onAdd(itemtitle){
+   this.todoService.addTitle(itemtitle.value);
+    itemtitle.value = null;
+  }//end onAdd
+
+  alterCheck(key:string,flag){
+    this.todoService.checkOrUnCheckTitle(key,!flag)
+  }//end alterCheck
+
+  onDelete($key){
+    this.todoService.removeTitle($key);
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAzS0y19qhK4FpoDoOifc6V_H_6tJUoNsY",
+    authDomain: "fir-ang-29eea.firebaseapp.com",
+    databaseURL: "https://fir-ang-29eea.firebaseio.com",
+    projectId: "fir-ang-29eea",
+    storageBucket: "fir-ang-29eea.appspot.com",
+    messagingSenderId: "116456756127"
+  }
+};
